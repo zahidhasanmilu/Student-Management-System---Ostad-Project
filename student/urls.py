1cea13ae31cd668160d643d09ab6607ac0df343a
zahidhasanmilu <minions.milu889@gmail.com>
2025-01-29 00:53:44 +0600

    Classbased UpdateVuew

--- student/urls.py
+++ student/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.Home.as_view(), name='index'),    
     path('students/', views.StudentListView.as_view(), name='all_student'),
     path('create_student/', views.StudentCreateView.as_view(), name='create_student'),
+    path('student/<int:id>/', views.StudentDetailView.as_view(), name='student_details'),
+    path('update/<int:id>/student/', views.StudentUpdateView.as_view(), name='update_student'),
     path('courses/', views.CourseList.as_view(), name='all_course'),
 ]
--- student/views.py
+++ student/views.py
@@ -1,11 +1,13 @@
+from .models import Student
+from django.views.generic.edit import CreateView
+from django.contrib import messages
+from student.forms import StudentForm
 from django.shortcuts import render, HttpResponse
 
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView,CreateView,DeleteView,ListView,UpdateView
+from django.views.generic import TemplateView, CreateView, DeleteView, ListView, UpdateView, DetailView
 
-from.models import Course, Student
-from student.forms import StudentForm
-from django.contrib import messages
+from .models import Course, Student
 
 
 # Create your views here.
@@ -13,69 +15,116 @@ from django.contrib import messages
 
 class Home(TemplateView):
     template_name = "index.html"
-    
+
     def get_queryset(self):
         students = Student.objects.all()
         courses = Course.objects.all()
         return students
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['students'] = self.get_queryset()
-        context['courses'] = Course.objects.all() 
+        context['courses'] = Course.objects.all()
         return context
-    
+
+
 class StudentListView(ListView):
     model = Student
     template_name = "students/students.html"
     context_object_name = 'students'
-    
-    
+
 
 # from django.contrib import messages
 # from django.urls import reverse_lazy
 # from django.views.generic import CreateView
 # from .models import Student, Course
 
+
 class StudentCreateView(CreateView):
     model = Student
     template_name = 'students/create_student.html'
-    success_url = reverse_lazy('all_student')  # Redirect to the student list page after successful submission
+    # Redirect to the student list page after successful submission
+    success_url = reverse_lazy('all_student')
+    fields = ['name', 'email', 'age', 'gender', 'phone',
+              'course', 'nationality', 'address', 'photo', 'active']
 
-    # Add the fields attribute
-    fields = ['name', 'email', 'age', 'gender', 'phone', 'course', 'nationality', 'address', 'photo', 'active']
 
     def form_valid(self, form):
-        # You don't need to manually assign the data since Django handles this automatically
-        # We only need to set the `user` field since it's not part of the form
-        form.instance.user = self.request.user  # Set the current user as the owner
-        
-        # Additional processing for active checkbox
-        if 'active' in self.request.POST:
-            form.instance.active = True
-        else:
-            form.instance.active = False
-        
-        # If needed, you can perform additional validations here
-        
-        # Call the parent class's form_valid to save the form
-        messages.success(self.request, 'Student has been successfully created!')
+        # Check if email already exists
+        form.instance.user = self.request.user
+        form.instance.active = 'active' in self.request.POST
+        name = form.data.get('name')
+        # Add a success message
+        messages.success(
+            self.request, f'{name} - Student has been successfully created!')
         return super().form_valid(form)
 
     def form_invalid(self, form):
-        # Add error message to user if the form is invalid
-        messages.warning(self.request, 'Student not created. Please check the form.')
-        return render(self.request, self.template_name, {'form': form})
+        # Check if the email already exists
+        # Use form.data to retrieve the submitted email
+        email = form.data.get('email')
+        if email and Student.objects.filter(email=email).exists():
+            messages.error(self.request, f'Email "{
+                           email}" already exists. Please use a different email.')
+
+        # # Add a general error message
+        # messages.error(
+        #     self.request, "Student creation failed. Please check the form for errors.")
+        # Re-render the form with existing data and errors
+        return self.render_to_response(self.get_context_data(form=form))
 
     def get_context_data(self, **kwargs):
         # Add available courses to context for the form
         context = super().get_context_data(**kwargs)
-        context['courses'] = Course.objects.all()  # Add all courses for the dropdown
+        # Add all courses for the dropdown
+        context['courses'] = Course.objects.all()
+        return context
+
+
+class StudentDetailView(DetailView):
+    model = Student
+    template_name = "students/student_details.html"
+    context_object_name ='student'
+    pk_url_kwarg  = 'id'
+    
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    template_name = "students/create_student.html"
+    fields = ['name', 'email', 'age', 'gender', 'phone', 'course', 'nationality', 'address', 'photo', 'active']
+    success_url = reverse_lazy('all_student')
+    pk_url_kwarg = 'id'
+    
+    def form_valid(self, form):
+        # Check if email already exists
+        form.instance.user = self.request.user
+        form.instance.active = 'active' in self.request.POST
+        
+        # Get the student's name and add a success message
+        name = form.instance.name
+        messages.success(self.request, f'{name} - Student has been successfully updated!')
+        
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        # Check if the email already exists
+        email = form.data.get('email')
+        if email and Student.objects.filter(email=email).exclude(pk=self.object.pk).exists():
+            messages.error(self.request, f'The email "{email}" is already registered. Please use a different one.')
+        # Re-render the form with existing data and errors
+        return self.render_to_response(self.get_context_data(form=form))
+    
+    def get_context_data(self, **kwargs):
+        # Add available courses to context for the form
+        context = super().get_context_data(**kwargs)
+        context['courses'] = Course.objects.all()
+        context['student_updates'] = True
         return context
     
-    
+
 
 class CourseList(ListView):
     model = Course
     context_object_name = 'courses_list'
-    template_name='students/courses.html'
+    template_name = 'students/courses.html'
+
